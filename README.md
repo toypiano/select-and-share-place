d41c602425b82c69a78e2ee523fcf8e987ec8ce4
Hayoun Lee <sidhlee@gmail.com>
2020-05-06 19:04:27 +0900

    Fetch coords for input address with axios

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ An app written with TypeScript
 Converts geo-coordinates into address and vice versa.
 
 - [Geocoding API](https://developers.google.com/maps/documentation/geocoding/start)
+
+## For API responses, declare types with only fields that we need
+
+When you get a response from an API, you typically want to access certain properties inside the data object and pass that to some other function that expects a certain type. For this, you might want to declare a type for the response data.
+
+When you define the type, you only need to define the fields that you're going to use, and you can reference the API documentation for expected values and type.
+
+- [Geocoding Response](https://developers.google.com/maps/documentation/geocoding/intro#GeocodingResponses)
+- [Status Codes](https://developers.google.com/maps/documentation/geocoding/intro#StatusCodes)
+
+```ts
+```
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,41 @@
+import axios from 'axios';
+
 const form = document.querySelector('form')! as HTMLFormElement;
 const addressInput = document.querySelector('#address')! as HTMLInputElement;
 
-function searchAddressHandler(e: Event) {
+async function searchAddressHandler(e: Event) {
   e.preventDefault();
   const enteredAddress = addressInput.value;
 
   const API_KEY = 'AIzaSyCigPEMEx6R4PJNn_dWAUKE_aJP_xxqhVU'; // cspell: disable-line
   // send this to Google's API!
+
+  // Only declare fields that we need
+  type GoogleGeocodingResponse = {
+    results: {
+      geometry: {
+        location: {
+          lat: number;
+          lng: number;
+        };
+      };
+    }[];
+    status: 'OK' | 'ZERO_RESULTS'; // only these two will do!
+  };
+  try {
+    const res = await axios.get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )},+CA&key=${API_KEY}`
+    );
+    if (res.data.status !== 'OK') {
+      throw new Error('Could not fetch location!');
+    }
+    const coords = res.data.results[0].geometry.location;
+  } catch (err) {
+    alert(err.message);
+    console.log(err);
+  }
 }
 
 form.addEventListener('submit', searchAddressHandler);
